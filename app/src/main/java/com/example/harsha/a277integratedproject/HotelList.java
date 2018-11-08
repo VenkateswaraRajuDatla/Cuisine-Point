afd7d6aa1a94bd7b0bd3d5154fd09499c4107967
Venkateswara Raju Datla <dvrajudatla1995@gmail.com>
2018-11-08 15:22:42 -0800

    Added UI for user session and activity

--- app/src/main/java/com/example/harsha/a277integratedproject/HotelList.java
+++ app/src/main/java/com/example/harsha/a277integratedproject/HotelList.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -58,6 +59,9 @@ public class HotelList extends AppCompatActivity {
 
         lvh.setAdapter(adapter);
 
+    }
+    public void onBack(View v){
+        onBackPressed();
     }
 
     }
